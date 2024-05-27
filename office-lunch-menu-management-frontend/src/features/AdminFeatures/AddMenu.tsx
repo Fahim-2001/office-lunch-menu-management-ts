8598e9c41024bf2fb4fc49b83d700de9f189a8b6
Fahim-2001 <mdyasirofficial2001@gmail.com>
2024-05-27 22:30:06 +0600

    style: Added styles to AddMenu feature section

--- office-lunch-menu-management-frontend/src/features/AdminFeatures/AddMenu.tsx
+++ office-lunch-menu-management-frontend/src/features/AdminFeatures/AddMenu.tsx
@@ -1,56 +1,63 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import "../../styles/AdminStyles.css";
 
 const AddMenu: React.FC = () => {
-  const [date, setDate] = useState<string>('');
-  const [options, setOptions] = useState<string>('');
+  const [date, setDate] = useState<string>("");
+  const [options, setOptions] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!date.trim() || !options.trim()) {
-      alert('Please fill in all fields.');
+      alert("Please fill in all fields.");
       return;
     }
     const newMenu = {
       date: date.trim(),
       options: options
         .trim()
-        .split(',')
+        .split(",")
         .map((option) => option.trim()),
     };
-    axios.post('http://localhost:3000/api/menus', newMenu).then((res) => {
+    axios.post("http://localhost:3000/api/menus", newMenu).then((res) => {
       if (res.status === 201) {
-        alert('Item added!');
+        alert("Item added!");
       }
     });
-    setDate('');
-    setOptions('');
+    setDate("");
+    setOptions("");
   };
 
   return (
-    <div className="add-menu">
+    <div className="add-menu-container">
       <h2>Add Daily Menu Options</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Date:</label>
+          <label className="add-menu-input-label">Date:</label>
           <input
             type="date"
             value={date}
             onChange={(e) => setDate(e.target.value)}
+            className="add-menu-input"
             required
           />
         </div>
-        <div style={{ marginTop: '10px' }}>
-          <label>Options (comma-separated):</label>
+        <div style={{ marginTop: "10px" }}>
+          <label className="add-menu-input-label">
+            Options (comma-separated):
+          </label>
           <input
             type="text"
             value={options}
             onChange={(e) => setOptions(e.target.value)}
             placeholder="e.g., Sandwich, Salad, Soup"
+            className="add-menu-input"
             required
           />
         </div>
-        <button type="submit">Add Menu</button>
+        <button type="submit" className="add-menu-btn">
+          Add Menu
+        </button>
       </form>
     </div>
   );

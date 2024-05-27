a2ec8333dc2f10e262f2631682b75341c2de833d
Fahim-2001 <mdyasirofficial2001@gmail.com>
2024-05-27 22:40:42 +0600

    style: Added styles to MenuList

--- office-lunch-menu-management-frontend/src/components/AdminComponents/MenuList.tsx
+++ office-lunch-menu-management-frontend/src/components/AdminComponents/MenuList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/AdminStyles.css'
 
 interface Menu {
   date: string;
@@ -12,13 +13,13 @@ interface MenuListProps {
 const MenuList: React.FC<MenuListProps> = ({ menus }) => {
   return (
     <div className="menu-list">
-      <h2>Menu List</h2>
+      <h2 className='menu-list-title'>Menu List</h2>
       {menus.length === 0 ? (
         <p>No menus available.</p>
       ) : (
-        <div>
+        <div >
           {menus.map((menu) => (
-            <div key={menu.date} className="menu-item">
+            <div key={menu.date} className='menu-list-items'>
               <h3>{new Date(menu.date).toDateString()}</h3>
               <p style={{ marginTop: '5px' }}>{menu.options.join(', ')}</p>
             </div>
